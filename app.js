d4c525240bfd37e4cb8ca1ab377f5a323ae3d1ff
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-21 19:35:47 +0100

    Modif

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let x = 6;
 document.getElementById("interval-start").addEventListener("click", () => timeOut());
 
 function timeOut() {
-    setTimeout(function () {
+    let time = setTimeout(function () {
         document.getElementById("interval-div").innerHTML = x;
         x+=6;
         if(x <= 60) {
             timeOut();
         }
     }, 6000);
+
+    document.getElementById("interval-stop").addEventListener("click", () => clearTimeout(time));
 }
